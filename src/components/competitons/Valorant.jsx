049580a6260fedbe568956a1c16385f10e9f2f55
Anup-Gandhi <anupgandhi81103@gmail.com>
2025-01-14 19:23:35 +0530

    online card

---
+++ src/components/competitons/Valorant.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import image1 from "./assets/Comp_valorant.jpg"; // Replace with the actual image for Humming
+import { Link } from "react-router-dom";
+import close from "./assets/cross.png";
+
+const Valorant = () => {
+    return (
+        <div className="w-[80vw] h-[80vh] laptop:h-[90vh] mx-[10vw] overflow-x-hidden overflow-y-scroll hide-scrollbar rounded-3xl laptop:my-[5vh] mobile:my-[10vh] fixed top-0 z-10 bg-white-custom ">
+            <Link to="../">
+                <button className="fixed top-[12vh] laptop:top-[8vh] right-[12vw] hover:scale-110 text-white text-lg self-center">
+                    <img src={close} alt="close" className="max-w-[10vw] h-7 z-50"/>
+                </button>
+            </Link>
+            <div className="pt-[5vh] laptop:pt-[9vh] grid h-[75vh]">
+                <div className="font-onejinja text-4xl text-center mb-2.5">
+                    Valorant
+                </div>
+                <img className="object-contain mx-[15vw] max-h-[60vh] w-[50vw]" src={image1} alt="" />
+                <button className="min-w-[20vw] p-[1%] mx-auto my-2.5 justify-self-center bg-[#C47C74] hover:bg-[#B90827] rounded-md transition-all duration-300 shadow-lg hover:scale-105 hover:shadow-xl ">
+                    Register Now
+                </button>
+                <div className="flex justify-center">
+                    <div className="font-appetite mx-auto">
+                        <div className="font-onejinja text-xl">Prize:</div>
+                        Winner: 10,000/-
+                        <br />
+                        Runner Up: 6,000/-
+                    </div>
+                    <div className="font-appetite mx-auto">
+                        Entry Fees:
+                        700/-
+                    </div>
+                </div>
+                <div className="w-[48vw] mx-auto mt-2.5">
+                    <div className="font-onejinja text-xl text-left mb-2.5">
+                        Guidlines:
+                    </div>
+                    <div className="font-appetite text-left mx-auto mb-2.5">
+                        1. Game will be in battle royale mode , all players must have latest version of VALORANT installed on their device with all classic maps downloaded. <br />
+                        2. Only in-game voice chat should be used after the game is started till its competition. <br />
+                        3. Hacking , Cheating , Use of unauthorized softwares are strictly prohabited. Players found violating these rules will be disquilified. <br />
+                        4.  Waiting time is atmost 15-20 minutes between games. If a team / player fails to join the room in time  we will not be responsible.<br />
+                        5. Competition will be held in online mode.<br />
+                        6. Maps will be random.<br />
+                        7.  By participating in the Valorant tournament organized by MIT AAROHAN players agree to abide by all tournament rules and regulations. The organizers reserve the right to modify rules, disqualify participants for misconduct, and resolve disputes at their discretion. Unauthorized third-party software is strictly prohibited. Prizes will be distributed according to the announced structure, and participants must provide accurate details for prize delivery.
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Valorant;
